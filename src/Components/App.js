46d13cffb0976008872fe7c5a6abfba84f90d924
Caleb335 <belac335@gmail.com>
2020-02-19 16:03:14 +0100

    added prerequisite components

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from 
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Maincontent />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
