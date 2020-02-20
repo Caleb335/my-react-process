2126585956eb1dbc4a721f41aac9b644f79ecafb
Caleb335 <belac335@gmail.com>
2020-02-20 19:35:50 +0100

    refixing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
 ReactDOM.render(
     <App />,
     document.getElementById("root")
 )
-
 
 
 
