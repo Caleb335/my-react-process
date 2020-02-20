6ddfac132230415cd7a1da317860980b87a8a7f2
Caleb335 <belac335@gmail.com>
2020-02-20 19:49:35 +0100

    added images

---
+++ src/Components/ContactCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactCard() {
+    return (
+        <div className="contact-card">
+            <img src="av1.png" />
+        </div>
+    )
+}
+
+export default ContactCard
